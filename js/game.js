5ede818361a17cc2a71d43d7d55d60dfc2ceb0a2
Guilherme Fonseca <gsbloogs194@gmail.com>
2023-07-16 16:37:58 -0300

    only some states win add

--- js/game.js
+++ js/game.js
@@ -1,17 +1,63 @@
 // iniciar minhas variaveis 
 let board = ['', '', '', '', '', '', '', '', '']
-
 let playerTime = 0;
-
-let symbols = [ '0', 'x']
-
+let symbols = [ 'o', 'x']
+let gameOver = false;
 //position da interface q é o id do .square elemento do html
 function handleMove (position) {
-    board[position] = symbols[playerTime];
 
-    if(playerTime == 0) {
-        playerTime = 1;
-    } else {
-        playerTime = 0;
+    //o true ou false faz nossa funcao executar sim ou nao dada a situacao
+    if (gameOver ) {
+        return;
     }
+
+    //se o id do quadrado igual vazio quero q receba o a posicao da vez
+    if (board[position] == "") {
+        //se ja tem o valor receba o msm valor
+        board[position] = symbols[playerTime];
+        
+        //houve jogada vencedora? se sim isSwin ganhou passar a ser gameover
+        gameOver = isWin(); 
+        //se o zero prenche "o" quadrado qro q passe a prencher o "x"
+        if (gameOver == false) {
+            if(playerTime == 0) {
+                playerTime = 1;
+            } else {
+                playerTime = 0;
+            }
+        }
+    }
+
+    return gameOver
+}
+
+function isWin() {
+    let winStates = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+//qro q nosso index 0 atualiza a largura e incremente cada acao
+    for (let i = 0; i < winStates.length; i++) {
+        //cada elemento significa uma posicao de qual sequeuncia
+        let seq = winStates[1];
+
+        let pos1 = seq[0];
+        let pos2 = seq[1];
+        let pos3 = seq[2];
+        //se positon dos 3 seguinte seguir esse padrao é win
+        if (board[pos1] == board[pos2] && 
+            board[pos1] == board[pos3] && 
+            board[pos1] != '') {
+                //temos um vencedor
+                return true;
+            }
+    }
+
+    return false
 }
--- js/interface.js
+++ js/interface.js
@@ -1,8 +1,8 @@
-//qro primeiro caregar pagina p saber onde o usuario clicar
+//qro primeiro varrer os quadrados e colocalos em arrays p saber a posicao
 document.addEventListener('DOMContentLoaded', () => {
     
     let squares = document.querySelectorAll(".square");
-    //cada quadrado qro q receba evento clicar
+    //cada quadrado qro q receba evento clicar enossa funcao de trocar de vez
     squares.forEach((square)=> {
         square.addEventListener('click', handleclick)
     })
@@ -10,13 +10,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function handleclick(event) {
 
-    //(target) pq qro elemento 1 sofreu acao da funcao
+    //(target) pq qro elemento q sofreu acao da funcao no caso o quadrado
     let square = event.target;
     let position = square.id; //qual position q qro q seja no board
 
-    handleMove(position);
-    updateSquares()
+    //vou pasasar por handle move qual nossa posicao
+    if(handleMove(position)) {
 
+        setInterval(() => {
+            alert("o jogo acabou o '" + playerTime + "' venceuu!!!")
+        }, 10);
+    }
+    updateSquares();
 }
 
 function updateSquares () {
@@ -28,7 +33,7 @@ function updateSquares () {
         let symbol = board[position]
 
         if(symbol != "") {
-            square.innerHTML = "<div class='${symbol}''></div>"
+            square.innerHTML = `<div class='${symbol}'></div>`
         }
     })
 }
